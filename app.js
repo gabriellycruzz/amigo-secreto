61b8ed0cbb7ca68797cdd266acf86bae098cb409
Gabrielly Cruz <gabrielly.cruz@aluno.ufca.edu.br>
2025-01-21 10:18:04 -0300

    Projeto Final

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 let amigos = [];
 
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
 function adicionarAmigo() {
+
     let inputAmigo = document.getElementById('amigo');
-    let nome = inputAmigo.value();
+    let nome = inputAmigo.value.trim();
 
     if (nome === '') {
         alert('Por favor, insira um nome.');
-        return;
+        return; 
     }
 
     amigos.push(nome);
-
-    inputAmigo.value = ''; 
+    inputAmigo.value = '';
+    atualizarLista();
 }
+
 
 function atualizarLista() {
     let listaAmigos = document.getElementById('listaAmigos');
@@ -22,8 +29,24 @@ function atualizarLista() {
     for (let i = 0; i < amigos.length; i++) {
         let li = document.createElement('li');
         li.textContent = amigos[i];
-
         listaAmigos.appendChild(li);
     }
 }
 
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert('Adicione pelo menos um amigo.');
+        return;
+    }
+
+    let indiceSorteado = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indiceSorteado];
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `Seu amigo secreto é: ${amigoSorteado}`;
+}
+
+function inicializar() {
+    exibirTextoNaTela('#resultado', '');
+}
+
+inicializar();
