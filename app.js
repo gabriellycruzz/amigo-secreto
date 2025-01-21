9e1e30c97efc7f7317d8b9bc7c341a303a531431
Gabrielly Cruz <gabrielly.cruz@aluno.ufca.edu.br>
2025-01-21 08:56:54 -0300

    Função agregar amigo

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaDeAmigos = [];
+
+function exibirTextoNaTela () {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+function exibirMensagemInicial() {
+    exibirTextoNaTela('h1', 'Lista de Amigo Secreto');
+    exibirTextoNaTela('p', 'Adicione um amigo para começar!');
+}
+
+function adicionarAmigo() {
+    let nome = document.querySelector('input').value;
+
+    if (nome == '') {
+        exibirTextoNaTela('p', 'Digite um nome válido!');
+    } else {
+        listaDeAmigos.push(nome);
+        exibirTextoNaTela('p', `Amigo ${nome} adicionado!`);
+        limparCampo();
+    }
+}
+
+function limparCampo() {
+    let campo = document.querySelector('input');
+    campo.value = '';
+}
+
+exibirMensagemInicial();
